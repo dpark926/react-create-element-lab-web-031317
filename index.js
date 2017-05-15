9ff71e92bd75e347b09aa3e9fad4918173d7b539
Daniel Park <dpeace926@gmail.com>
2017-05-14 22:00:26 -0400

    Done.

--- index.js
+++ index.js
@@ -15,28 +15,3 @@ ReactDOM.render(
   meInReact,
   document.getElementById('main')
 );
-//
-// const list =
-//   React.createElement('div', {},
-//     React.createElement('h1', {}, 'My favorite ice cream flavors'),
-//     React.createElement('ul', {},
-//       [
-//         React.createElement('li', { className: 'brown' }, 'Chocolate'),
-//         React.createElement('li', { className: 'white' }, 'Vanilla'),
-//         React.createElement('li', { className: 'yellow' }, 'Banana')
-//       ]
-//     ));
-//
-// ReactDOM.render(list, document.getElementById('main'));
-//
-// <div class="me">
-//   <h1>An Awesome Person</h1>
-//   <p>Who is learning React</p>
-//
-//   <ul class="me__interests">
-//     <li>JavaScript</li>
-//     <li>React</li>
-//     <li>Movies</li>
-//     <li>Ice cream</li>
-//   </ul>
-// </div>
